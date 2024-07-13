993e1b699f111e665ba08159cbb642b5b0b831c1
sdmexkir <102284398+Kopal777@users.noreply.github.com>
2024-07-13 11:58:03 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'https://fs-booking-application-frontend.vercel.app/'
 }))
 
 mongoose.connect(process.env.MONGO_URL)
